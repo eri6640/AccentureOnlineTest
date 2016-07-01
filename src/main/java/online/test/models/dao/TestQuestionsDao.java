5556e6fabcee3286b1bc961e9c38dbf680fc07ef
Lind_sy <Serenetija91@inbox.lv>
2016-07-01 15:26:08 +0300

    Added Dao interfaces

---
+++ src/main/java/online/test/models/dao/TestQuestionsDao.java
@@ -0,0 +1,18 @@
+package online.test.models.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import online.test.models.TestOuestions;
+
+@Transactional
+public interface TestQuestionsDao extends CrudRepository<TestOuestions, Long> {
+	
+	@Query("select q from TestOuestions q inner join q.tests as t where t.id = :testID")
+	public List<TestOuestions> getCurrentTestQuestions(Long testID);
+	
+}
