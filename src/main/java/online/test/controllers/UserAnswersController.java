c04cbf18b7facb506630f551ba2a6613b8fc5016
Peteris <itmuser@osboxes>
2016-07-05 12:01:09 +0100

    Add/Remove functions for Questions/Answers controllers

--- src/main/java/online/test/controllers/UserAnswersController.java
+++ src/main/java/online/test/controllers/UserAnswersController.java
@@ -1,6 +1,9 @@
 package online.test.controllers;
 
 
+import online.test.models.TestQuestions;
+import online.test.models.Tests;
+import online.test.models.User;
 import online.test.models.UserAnswers;
 import online.test.models.dao.UserAnswersDao;
 
@@ -12,12 +15,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserAnswersController {
 	
+	
+	
 	@RequestMapping("/data/useranswers/selectall")
 	@ResponseBody
 	public Iterable<UserAnswers> selectAll() {
 		Iterable<UserAnswers> list = userAnswersDao.findAll();
 		return list;
+	} 
+	
+	@RequestMapping("/data/useranswers/create")
+	@ResponseBody
+	public String addAnswer(TestQuestions testsQuestions, Tests tests, User user, String answer, byte[] imageAnswer) {
+		UserAnswers questionAnswer = null;
+		try {
+			questionAnswer = new UserAnswers(testsQuestions, tests, user, answer, imageAnswer);
+			userAnswersDao.save(questionAnswer);
+		}
+		catch (Exception ex) {
+			return "Error adding answer: " + ex.toString();
+		}
+		return "Answer succesfully added!";
 	}
+	
+	@RequestMapping("/data/useranswers/delete")
+	@ResponseBody
+	public String removeAnswer(TestQuestions testsQuestions, Tests tests, User user, String answer, byte[] imageAnswer) {
+		try {
+			UserAnswers questionAnswer = new UserAnswers(testsQuestions, tests, user, answer, imageAnswer);
+			userAnswersDao.delete(questionAnswer);
+		}
+		catch (Exception ex) {
+			return "Error deleting the answer: " + ex.toString();
+		}
+		return "Answer succesfully deleted!";
+	}
+	
 	@Autowired  
 	private UserAnswersDao userAnswersDao;
 } //UserAnswersController end
