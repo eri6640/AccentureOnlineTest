ddcec9e1fd3ed4652e093610c9bf189cb4f08d42
eri6640 <eri6640@gmail.com>
2016-07-06 14:35:11 +0300

    TestsDao error removed

--- src/main/java/online/test/models/dao/TestsDao.java
+++ src/main/java/online/test/models/dao/TestsDao.java
@@ -11,10 +11,7 @@ public interface TestsDao extends CrudRepository<Tests, Long> {
 
 	//@Query("from Tests")
 	//public List<Tests> getAllTests();
-<<<<<<< HEAD
 	
 	public Tests findById( long id );
-=======
->>>>>>> refs/remotes/origin/lbranch
 
 }
