57a60940c93fc3a44cbb6feb0192bc93bb9809bc
eri6640 <eri6640@gmail.com>
2016-07-08 12:01:17 +0300

    question page update

--- src/main/java/online/test/controllers/UserAnswersController.java
+++ src/main/java/online/test/controllers/UserAnswersController.java
@@ -2,20 +2,32 @@ package online.test.controllers;
 
 
 import online.test.models.TestQuestions;
-import online.test.models.Tests;
 import online.test.models.User;
 import online.test.models.UserAnswers;
 import online.test.models.dao.UserAnswersDao;
+import online.test.utils.MainUtils;
+import online.test.utils.TestQuestionsUtils;
+import online.test.utils.UserUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserAnswersController {
 	
+	@Autowired  
+	private UserAnswersDao userAnswersDao;
 	
+	@Autowired
+	TestQuestionsUtils questionsUtils = new TestQuestionsUtils();
+	@Autowired
+	UserUtils userUtils = new UserUtils();
+	MainUtils utils = new MainUtils();
 	
 	@RequestMapping("/data/useranswers/selectall")
 	@ResponseBody
@@ -24,6 +36,27 @@ public class UserAnswersController {
 		return list;
 	}
 	
-	@Autowired  
-	private UserAnswersDao userAnswersDao;
+	@RequestMapping("/data/tests/saveAnswer")
+	@ResponseBody
+	public boolean saveAnswer( @RequestParam("questionId") String questionId_string, @RequestParam("answer") String answer_string, HttpServletRequest request ) {
+		
+		if( questionId_string.isEmpty() || answer_string.isEmpty() ) return false;
+		
+		User user_user = null;
+		if( ( user_user = userUtils.getUserFromRequest( request ) ) == null ){
+			utils.showThis( "User null" );
+			return false;
+		}
+		
+		TestQuestions question = questionsUtils.getQuestion( questionId_string );
+		
+		if( question == null ) return false;
+		
+		if( answer_string.startsWith( "[" ) && answer_string.endsWith( "]" ) ){
+			answer_string = answer_string.replace( "[", "").replace( "]", "");
+		}
+
+		return questionsUtils.saveAnswer( user_user, question, answer_string );
+	}
+	
 } //UserAnswersController end
--- src/main/java/online/test/models/dao/TestQuestionsDao.java
+++ src/main/java/online/test/models/dao/TestQuestionsDao.java
@@ -9,11 +9,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import online.test.models.TestQuestions;
+import online.test.models.Tests;
 
 @Transactional
 public interface TestQuestionsDao extends CrudRepository<TestQuestions, Long> {
 
 	@Query("select q from TestQuestions q inner join q.tests as t where t.id = :testID")
 	public List<TestQuestions> getCurrentTestQuestions(@Param("testID")Long testID);
+	
+	public TestQuestions findById( long id );
 
 }
