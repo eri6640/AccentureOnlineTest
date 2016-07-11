3cc874384a58a36d1c1daca3d4d31fdb3885af6b
eri6640 <eri6640@gmail.com>
2016-07-11 13:57:43 +0300

    save img - enabled

--- src/main/resources/static/resources/js/coreapp.js
+++ src/main/resources/static/resources/js/coreapp.js
@@ -347,8 +347,8 @@ app.controller( 'QuestionController', function( $rootScope, $scope, $http, $loca
 			case 4:
 				var id = document.getElementById("sampleBoard");
 			    var img = id.toDataURL("image/png");
-			    //$scope.saveAnswer( img );
-			    $scope.saveAnswer( "image" );
+			    $scope.saveAnswer( img );
+			    //$scope.saveAnswer( "image" );
 				break;
 			default:
 				//dsfg
